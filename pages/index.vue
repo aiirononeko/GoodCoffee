84e2e7c8009e17f3865a9b3dafbffa7c06a67ca1
片田凌太 <aiirononeko@gmail.com>
2020-06-12 01:20:01 +0900

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="container">
+    <div>
+      <h2 class="title">
+        さあ、カッピングをはじめよう
+      </h2>
+      <div class="user_form">
+        <button @click="goSignin" class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l user_form_items">ログイン</button>
+        <p class="user_form_items">または、</p>
+        <button @click="goSignup" class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l user_form_items">アカウントを作成</button>
+      </div>
+      <div class="links">
+        <nuxt-link to="/">利用規約</nuxt-link>
+        <nuxt-link to="/">プライバシーポリシー</nuxt-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  methods: {
+    goSignin() {
+      this.$router.push("/signin")
+    },
+    goSignup() {
+      this.$router.push("/signup")
+    }
+  }
+})
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 80px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.user_form {
+  margin-top: 80px;
+  margin-bottom: 20px;
+}
+
+.user_form_items {
+  margin-top: 20px;
+}
+
+.links {
+  margin-top: 50px;
+}
+
+.links > a:hover {
+  color: blue;
+}
+</style>
---
+++ pages/signin.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <h2>ログインしましょう</h2>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 80px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+</style>
