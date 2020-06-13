e87ed42159d3e5b7f0f9fd3aef3489275075e3f1
片田凌太 <aiirononeko@gmail.com>
2020-06-13 11:39:48 +0900

    ログイン画面のレイアウトを調整

--- pages/signin.vue
+++ pages/signin.vue
@@ -3,13 +3,37 @@
     <div>
       <h2 class="title">ログインしましょう</h2>
     </div>
-    <div class="signin_form">
-      <label for="email">メールアドレス</label>
-      <input type="text" v-model="email"><br>
-      <label for="password">パスワード</label>
-      <input type="password" v-model="password"><br>
-      <button @click="loginWithEmail" class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l user_form_items">ログインする</button>
-    </div>
+    
+    <form class="w-full max-w-sm my-8">
+      <div class="md:flex md:items-center mb-6">
+        <div class="md:w-1/3">
+          <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="email">
+            Email
+          </label>
+        </div>
+        <div class="md:w-2/3">
+          <input v-model="email" placeholder="taro@example.com" class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="email" type="text">
+        </div>
+      </div>
+      <div class="md:flex md:items-center mb-6">
+        <div class="md:w-1/3">
+          <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-username">
+            Password
+          </label>
+        </div>
+        <div class="md:w-2/3">
+          <input v-model="password" class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-username" type="password" placeholder="******************">
+        </div>
+      </div>
+      <div class="my-8">
+        <div>
+          <button @click="loginWithEmail" class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+            ログインする
+          </button>
+        </div>
+      </div>
+    </form>
+
     <nuxt-link to="/signup">アカウントをお持ちでない方はこちら</nuxt-link>
     <nuxt-link to="/">トップページに戻る</nuxt-link>
   </div>
@@ -60,12 +84,46 @@ export default {
     'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
   display: block;
   font-weight: 300;
-  font-size: 30px;
+  font-size: 40px;
   color: #35495e;
   letter-spacing: 1px;
 }
 
-.signin_form > input {
-  border: 1px black solid;
+form {
+  margin-top: 80px;
+}
+
+a {
+  margin-top: 10px;
+}
+
+a:hover {
+  color: #3490dc;
+}
+
+/** 
+ * タブレット用ブレークポイント
+ */
+@media screen and (max-width: 1179px) {
+  .title {
+    font-size: 40px;
+  }
+
+  .user_form_items {
+    width: 30%;
+  }
+}
+
+/**
+ * スマホ用ブレークポイント
+ */
+@media screen and (max-width: 767px) {
+  .title {
+    font-size: 25px;
+  }
+
+  .user_form_items {
+    width: 60%;
+  }
 }
 </style>
